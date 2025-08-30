0af9455d54617c540bfe914331bd09455208392f
riteshregar31 <riteshregar31@gmail.com>
2025-08-30 19:13:27 +0530

    added header and footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ListTodoComponent from './components/ListTodoComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
 
 function App() {
 
 
   return (
     <>
+    <HeaderComponent/>
      <ListTodoComponent/>
+     <FooterComponent/>
     </>
   )
 }
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <p className='text-center'>
+Have a nice day!
+            </p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
